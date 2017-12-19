d904c928970095e3c8c52f955fd8f27fc2adc17a
Stefan Dilger <dilgers24@gmail.com>
2017-12-19 20:12:03 +0100

    Finished assinment 5 all ex files

--- Assignment_5/ho_optimization/ho_exercise.py
+++ Assignment_5/ho_optimization/ho_exercise.py
@@ -1,9 +1,16 @@
 import pickle
 import numpy as np
+import os
+from copy import deepcopy
 
 
-rf = pickle.load(open("./rf_surrogate_cnn.pkl", "rb"))
-cost_rf = pickle.load(open("./rf_cost_surrogate_cnn.pkl", "rb"))
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+rf_surrogate_cnn_file = os.path.join(THIS_FOLDER, 'rf_surrogate_cnn.pkl')
+rf_cost_surrogate_cnn = os.path.join(THIS_FOLDER, 'rf_cost_surrogate_cnn.pkl')
+
+
+rf = pickle.load(open(rf_surrogate_cnn_file, "rb"))
+cost_rf = pickle.load(open(rf_cost_surrogate_cnn, "rb"))
 
 
 def objective_function(x, epoch=40):
